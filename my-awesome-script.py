dca387499ea7546b7aa229677b136edaec9fc8b4
Jainish-Bharti <jainish.bharti@datagrokr.com>
2022-04-23 15:47:34 +0530

    Added PR details to my-awesome-script

--- my-awesome-script.py
+++ my-awesome-script.py
@@ -4,3 +4,12 @@ print('Howdy World!')
 
 print('Hajimemashite sekai!')
 
+
+#Title: Allow customer notification via WhatsApp
+#Decription:
+#Customers have been requesting the ability to get
+#notified about product updates via their WhatsApp number.
+#This mode of notification is optional, but if specified, should cause
+#notifications to go out both on their emails as well as WhatsApp numbers.
+
+
